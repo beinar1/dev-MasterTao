047108033e6aa8f29dde57017a7b6fc16745b5a5
beinar1 <sasha.beynario@gmail.com>
2024-07-01 15:47:19 +0200

    fonts commit

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,1 +1,8 @@
-  
+  const burger = document.querySelector('.btn-burger');
+  const mobileContainer = document.querySelector('.mobile-container')
+
+  function addClass(){
+        mobileContainer.classList.toggle('show')
+  }
+
+  burger.addEventListener('click', addClass)
