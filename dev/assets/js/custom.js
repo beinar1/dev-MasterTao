d98662cefad9e4d73c13dbdce98c7cf4d30e8a79
beinar1 <sasha.beynario@gmail.com>
2024-07-01 18:02:55 +0200

    third commit

--- dev/assets/js/custom.js
+++ dev/assets/js/custom.js
@@ -1,8 +1,11 @@
   const burger = document.querySelector('.btn-burger');
   const mobileContainer = document.querySelector('.mobile-container')
+  const body = document.querySelector('body')
 
-  function addClass(){
+  function toggleClass(){
         mobileContainer.classList.toggle('show')
+        burger.classList.toggle('active')
+        body.classList.toggle('overflow-hidden')
   }
 
-  burger.addEventListener('click', addClass)
+  burger.addEventListener('click', toggleClass)
